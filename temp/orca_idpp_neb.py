43ace501bb2d984cb2b20b59aac6dc4909e82135
Martin Lea <you@example.com>
2021-09-14 11:27:43 +0100

    NEB_input script added

---
+++ temp/orca_idpp_neb.py
@@ -0,0 +1,20 @@
+import os,shutil
+for i in ["%1d" % i for i in range(1,7)]:
+    os.mkdir(i+'/NEB')
+    os.chdir(i+'/NEB')
+    shutil.copy(i+'/trans/optimized.xyz', './start.xyz')   
+    shutil.copy(i+'/cis/optimized.xyz', './end.xyz')
+    inp = open(i+'/NEB/input.inp', 'w+')
+    inp.write('! B3LYP D3BJ def2-TZVP RIJCOSX \n'
+              '! NEB \n'
+              '%pal nprocs 48 end \n'
+              '%base "neb" \n'
+              '%neb NEB_END_XYZFile "end.xyz" \n'
+              'NImages 11 \n'
+              'Monitor_Internals { D 12 7 14 15 } end \n'
+              'end \n'
+              '* xyzfile 0 1 start.xyz')
+    inp.close()
+    os.system('python ~/subgen-2.py '+i+'-neb 1 orca501 4 m.lea')
+    os.system('sbatch submit.sh')
+    os.chdir('../..')
