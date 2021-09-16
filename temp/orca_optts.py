27e949723af96da5c27c6c619ab01f10bbdd3e9d
Martin Lea <you@example.com>
2021-09-16 13:26:13 +0100

    added the OptTS script

---
+++ temp/orca_optts.py
@@ -0,0 +1,15 @@
+import os,shutil
+for i in ["%1d" % i for i in range(1,7)]:
+    os.mkdir(i+'/NEB/ts-opt/')
+    os.chdir(i+'/NEB/ts-opt/')
+    shutil.copy(i+'/NEB/neb_NEB-HEI_converged.xyz', './ts.xyz')   
+    inp = open(i+'/NEB/input.inp', 'w+')
+    inp.write('! B3LYP D3BJ def2-TZVP RIJCOSX \n'
+              '! OptTS \n'
+              '%pal nprocs 48 end \n'
+              '%base "tsopt" \n'
+              '* xyzfile 0 1 ts.xyz \n')
+    inp.close()
+    os.system('python ~/subgen-2.py '+i+'-ts 1 orca501 48 m.lea')
+    os.system('sbatch submit.sh')
+    os.chdir('../../../')
